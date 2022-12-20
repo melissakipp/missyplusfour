019b76e3e2184bd3232f7ef7290a5f563357b0a5
Melissa Kipp <melissakipp.az@gmail.com>
2022-12-20 08:43:32 -0700

    [Update] Add Spring Security
    Implemented BCrytPasswordEncoder
    Temporarily disabling security figures

--- missyplusfour/src/main/java/com/missyplusfour/missyplusfour/MissyplusfourApplication.java
+++ missyplusfour/src/main/java/com/missyplusfour/missyplusfour/MissyplusfourApplication.java
@@ -2,8 +2,10 @@ package com.missyplusfour.missyplusfour;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// Temporarily disabling security figures
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MissyplusfourApplication {
 
 	public static void main(String[] args) {

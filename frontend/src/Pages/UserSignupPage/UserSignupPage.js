9043d2863778cd639094e9b61ce330a7f4f36817
Melissa Kipp <melissakipp.az@gmail.com>
2022-12-23 14:47:16 -0700

    [Update] Add Sass

--- frontend/src/Pages/UserSignupPage/UserSignupPage.js
+++ frontend/src/Pages/UserSignupPage/UserSignupPage.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
 
-import styles from './UserSignupPage.module.css';
+import styles from './UserSignupPage.module.scss';
 
 export class UserSignupPage extends Component {
 

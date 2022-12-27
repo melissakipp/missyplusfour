8d41698dcd730a28f444500d61468742375a42ce
Melissa Kipp <melissakipp.az@gmail.com>
2022-12-27 16:10:04 -0700

    [Beginning] Clean up and add font

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Code Clean Up</h1>
+      <code>div className="App"</code>
+      <p>I became passionate about design and development when I started my search for a new career. I am a self-taught programmer that has recently completed the Java Web Developer program with Merit America (Nov 2022). I am passionate about designing and developing the web. Currently, I enjoy building Java/Spring Boot/ReactJS applications with an Agile approach. I am an adaptable self-motivated learner who believes in continual self-improvement and loves to learn about new technologies.</p>
     </div>
   );
 }

732b65aa53bfb46bdd808d2882a79ac71d043340
Melissa Kipp <melissakipp.az@gmail.com>
2022-12-27 15:17:08 -0700

    [New-Starting-Point] Reinstalled clean CRA (bug w/tests)

--- frontend/src/api/apiCalls.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios"
-
-export const signup = (user) => {
-  return axios.post('/api/1.0/users', user);
-}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/index.css';
-
+import './index.css';
 import App from './App';
-
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
